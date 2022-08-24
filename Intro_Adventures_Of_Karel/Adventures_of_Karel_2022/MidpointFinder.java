2678af2acb89fc1030397d83ddbc39a3fe7b60dc
Daroshi11260 <rytaneja@gmail.com>
2022-08-24 13:08:37 -0700

    added Midpoint Classwork

--- Intro_Adventures_Of_Karel/Adventures_of_Karel_2022/MidpointFinder.java
+++ Intro_Adventures_Of_Karel/Adventures_of_Karel_2022/MidpointFinder.java
@@ -5,7 +5,35 @@ public class MidpointFinder extends Karel
 
     public void run()
     {
-        
+        layRowOfBeepers(); //leave first and last space empty :)
+        //you must start on a beeper
+        while(beepersPresent()){
+            pickUpLastBeeperAtOtherEnd(); //reposition so you are on a beeper
+        }
+        turnAround();
+        move();
+        turnAround();
     }
-
+    public void layRowOfBeepers(){ //for laying row with first and last empty
+        move();
+        while(frontIsClear()){
+            putBeeper();
+            move();
+        }
+        turnAround();
+        move();
+    }
+    public void turnAround(){ //turn around -_-
+        turnLeft();
+        turnLeft();
+    }
+    public void pickUpLastBeeperAtOtherEnd(){ // just move and run to "end of line" meaning there is empty space at the end
+        while(beepersPresent() && frontIsClear()){
+            move();
+        }
+        turnAround();
+        move();
+        pickBeeper();
+        move();
+    }
 }
