9cf305a06f42068c8a67d0eaaca51130b4750116
Daroshi11260 <rytaneja@gmail.com>
2022-08-24 15:02:05 -0700

    SpringTime Homework

--- Intro_Adventures_Of_Karel/Adventures_of_Karel_2022/SpringTime.java
+++ Intro_Adventures_Of_Karel/Adventures_of_Karel_2022/SpringTime.java
@@ -5,7 +5,56 @@ public class SpringTime extends Karel
 
     public void run()
     {
-
+        for (int i=0; i < 5; i++){
+            encounterTree();
+            ascendTree();
+            plantPetals();
+            move();
+            descendTree();
+        }
+    }
+    
+    public void encounterTree() {
+        while (frontIsClear()){
+            move();
+        }
+    }
+    
+    public void ascendTree() {
+        turnLeft();
+        while(rightIsBlocked()){
+            move();
+        }
+        turnRight();
+    }
+    
+    public void descendTree() {
+        turnRight();
+        while (frontIsClear()){
+            move();
+        }
+        turnLeft();
     }
 
+    public void turnRight() {
+        turnLeft();
+        turnLeft();
+        turnLeft();
+    }
+    
+    public void plantPetals() {
+        putBeeper();
+        move();
+        putBeeper();
+        turnLeft();
+        move();
+        putBeeper();
+        turnLeft();
+        move();
+        putBeeper();
+        turnLeft();
+        move();
+        turnLeft();
+        
+    }
 }
