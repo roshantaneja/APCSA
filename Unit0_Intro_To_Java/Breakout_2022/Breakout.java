45d2d2f7ed0696f4adf8bafbdef792ca8470fc3d
Daroshi11260 <rytaneja@gmail.com>
2022-09-13 09:38:22 -0700

    Breakout program random shenanigans

--- Unit0_Intro_To_Java/Breakout_2022/Breakout.java
+++ Unit0_Intro_To_Java/Breakout_2022/Breakout.java
@@ -9,7 +9,7 @@ import java.awt.event.*;
 public class Breakout extends GraphicsProgram 
 {
 
-/** Width and height of application window in pixels */
+    /** Width and height of application window in pixels */
     public static final int APPLICATION_WIDTH = 400;
     public static final int APPLICATION_HEIGHT = 600;
 
@@ -17,47 +17,52 @@ public class Breakout extends GraphicsProgram
     private static final int WIDTH = APPLICATION_WIDTH;
     private static final int HEIGHT = APPLICATION_HEIGHT;
 
-/** Dimensions of the paddle */
+    /** Dimensions of the paddle */
     private static final int PADDLE_WIDTH = 60;
     private static final int PADDLE_HEIGHT = 10;
 
-/** Offset of the paddle up from the bottom */
+    /** Offset of the paddle up from the bottom */
     private static final int PADDLE_Y_OFFSET = 30;
 
-/** Number of bricks per row */
+    /** Number of bricks per row */
     private static final int NBRICKS_PER_ROW = 10;
 
-/** Number of rows of bricks */
+    /** Number of rows of bricks */
     private static final int NBRICK_ROWS = 10;
 
-/** Separation between bricks */
+    /** Separation between bricks */
     private static final int BRICK_SEP = 4;
 
-/** Width of a brick */
+    /** Width of a brick */
     private static final int BRICK_WIDTH =
-      (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
+        (WIDTH - (NBRICKS_PER_ROW - 1) * BRICK_SEP) / NBRICKS_PER_ROW;
 
-/** Height of a brick */
+    /** Height of a brick */
     private static final int BRICK_HEIGHT = 8;
 
-/** Radius of the ball in pixels */
+    /** Radius of the ball in pixels */
     private static final int BALL_RADIUS = 10;
 
-/** Offset of the top brick row from the top */
+    /** Offset of the top brick row from the top */
     private static final int BRICK_Y_OFFSET = 70;
-/** Number of "lives" (balls) before the player loses */
+    /** Number of "lives" (balls) before the player loses */
     private static final int NUM_LIVES = 3;
 
- /** Global variables declared here.  You should feel free to add others as needed. */
-     GRect paddle;
-     GOval ball;
-     double vx;
-     double vy;
+    /** Global variables declared here.  You should feel free to add others as needed. */
+    GRect paddle;
+    GOval ball;
+    double vx;
+    double vy;
+    State state;
     
-/** Runs the Breakout program. */
+    public enum State {
+        MENU, GAME, END
+    }
+
+    /** Runs the Breakout program. */
     public void run() 
     {
-
+        
     }
-   
+
 }
