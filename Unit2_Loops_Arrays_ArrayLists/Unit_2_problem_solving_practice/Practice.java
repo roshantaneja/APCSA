15f5a862270c659fd12594c644ef50a960b5922d
Daroshi11260 <rytaneja@gmail.com>
2022-11-16 12:16:35 -0800

    Unit to Problem solving practice

---
+++ Unit2_Loops_Arrays_ArrayLists/Unit_2_problem_solving_practice/Practice.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+
+public class Practice
+{
+    public boolean containsNoWordsShorterThanFiveLetters(String[] words)
+    {
+        return false; // just to get it to compile
+    }
+
+    public void removeAllCatWords(ArrayList<String> words)
+    {
+
+    }
+
+    public int countAdjacentMultiplesOfTen(ArrayList<Integer> nums)
+    {
+        return 0;  // just to get it to compile
+    }
+
+    public boolean sameNumberOfFiveAndSevenFactors(int[] nums)
+    {
+        return false; // just to get it to compile
+    }
+
+    public boolean hasWordBetween5and10LettersLong(String[] words)
+    {
+        return false; // just to get it to compile
+    }
+
+    public boolean containsKwordAndMword(ArrayList<String> words)
+    {
+        return false; // just to get it to compile
+    }
+        
+    public int biggestEvenNumber(int[] nums)
+    {
+        return -1; // just to get it to compile
+    }
+    
+    public ArrayList<String> getAllWordsSameLengthAsFirstWord(String[] words)
+    {
+        return null; // just to get it to compile
+    }
+}
