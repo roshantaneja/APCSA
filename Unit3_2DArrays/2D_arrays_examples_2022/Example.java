cdf66ed8d8c17697180f5b61ea71ef1bbc538fa4
Daroshi11260 <rytaneja@gmail.com>
2022-12-02 14:15:59 -0800

    first he found a bug in production, what happens next will SHOCK you!

--- Unit3_2DArrays/2D_arrays_examples_2022/Example.java
+++ Unit3_2DArrays/2D_arrays_examples_2022/Example.java
@@ -5,6 +5,36 @@ public class Example extends ConsoleProgram
 {
     public void run()
     {
+        int[][] nums = new int[10][12];
+        
+        for (int r = 0; r<nums.length;r++){
+            for (int c = 0; c < nums[r].length; c++){
+                nums[r][c] = (int)(Math.random()*25);
+            }
+        }
+        
+        nums[0][0] = 3;
+        nums[0][11] = 3;
+        nums[9][0] = 3;
+        nums[9][11] = 3;
+        
+        int counter = 0;
+        for (int r = 0; r<nums.length;r++){
+            for (int c = 0; c < nums[r].length; c++){
+                if (nums[r][c] == 3){
+                    counter++;
+                }
+                if (nums[r][c] / 10 == 0){
+                    print(nums[r][c] + "  ");
+                }else {
+                    print(nums[r][c] + " ");
+                }
+            }
+            println();
+        }
+        
+        println();
+        println(counter);
         
     }
 }
