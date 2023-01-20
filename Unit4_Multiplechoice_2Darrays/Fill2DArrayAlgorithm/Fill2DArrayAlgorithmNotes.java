f0ea4ab68e28ef8f0040eae31205a4dca0a9668e
Daroshi11260 <rytaneja@gmail.com>
2023-01-20 14:00:08 -0800

    tiles work

--- Unit4_Multiplechoice_2Darrays/Fill2DArrayAlgorithm/Fill2DArrayAlgorithmNotes.java
+++ Unit4_Multiplechoice_2Darrays/Fill2DArrayAlgorithm/Fill2DArrayAlgorithmNotes.java
@@ -2,34 +2,92 @@
 
 public class Fill2DArrayAlgorithmNotes
 {
-
     public int[][] getGridOfNumbersUsingCounterStartingAtOne(int rows, int cols)
     {
-       
+       int[][] result = new int[rows][cols];
+       int counter = 1;
+       for (int r = 0; r < rows; r++){
+           for (int c = 0; c < cols; c++){
+               result[r][c] = counter;
+               counter ++;
+           }
+       }
+       return result;
     }
     
     public int[][] getGridOfNumbersUsingCounter(int rows, int cols)
     {
-       
+       int[][] result = new int[rows][cols];
+       int counter = 0;
+       for (int r = 0; r < rows; r++){
+           for (int c = 0; c < cols; c++){
+               result[r][c] = counter;
+               counter ++;
+           }
+       }
+       return result;
     }
 
     public int[][] getGridOfNumbersUsingMath(int rows, int cols)
     {
+       int[][] result = new int[rows][cols];
+       for (int r = 0; r < rows; r++){
+           for (int c = 0; c < cols; c++){
+               result[r][c] = r * cols + c;
+           }
+       }
+       return result;
     }
     
     public int[][] getGridOfNumbersUsingMathColumnMajorOrder(int rows, int cols)
     {
+       int[][] result = new int[rows][cols];
+       for (int c = 0; c < cols; c++){
+           for (int r = 0; r < rows; r++){
+               result[r][c] = c * rows + r;
+           }
+       }
+       return result;
     }
     
     public String[][] getGridOfWordsUsingMath (int rows, int cols, String[] words)
     {
+       String[][] result = new String[rows][cols];
+       int counter = 0;
+       for (int r = 0; r < rows; r++){
+           for (int c = 0; c < cols; c++){
+               result[r][c] = words[counter];
+               counter++;
+           }
+       }
+       return result;
     }
     
     public String[][] getGridOfWordsUsingMathIfPresent (int rows, int cols, String[] words)
     {
+       String[][] result = new String[rows][cols];
+       for (int r = 0; r < rows; r++){
+           for (int c = 0; c < cols; c++){
+               int index = r * cols + c;
+               if (index > words.length){
+                   result[r][c] = words[index];
+               }
+           }
+       }
+       return result;
     }
     
     public String[][] getGridOfWordsColumnMajorUsingMathIfPresent (int rows, int cols, String[] words)
     {
+       String[][] result = new String[rows][cols];
+       for (int c = 0; c < cols; c++){
+           for (int r = 0; r < rows; r++){
+               int index = c * rows + r;
+               if (index > words.length){
+                   result[r][c] = words[index];
+               }
+           }
+       }
+       return result;
     }
 }
---
+++ Unit4_Multiplechoice_2Darrays/Tiles_2023/Tiles.java
@@ -0,0 +1,124 @@
+
+import acm.program.*;
+import acm.graphics.*;
+import java.awt.Color;
+import java.awt.event.*;
+
+public class Tiles extends GraphicsProgram
+{
+    private static final int TILE_SIZE = 80;
+    private static final int NUM_ROWS = 5;
+    private static final int NUM_COLS = 6;
+    private static final Color firstColor = Color.red;
+    private static final Color secondColor = Color.black;
+
+    private GRect[][] tiles;
+    private int status;
+
+    public void run()
+    {
+        setSize(TILE_SIZE*NUM_COLS, TILE_SIZE*NUM_ROWS+24);
+        status = -1;
+        initializeTiles();
+        status = playGame(); 
+        String[] endGameStrings = {"All same color", "Checkerboard", "Ring", "Stripes"};
+        showEndMessage(endGameStrings[status]);
+    }
+
+    private void showEndMessage(String message)
+    {
+        GLabel label = new GLabel(message, 0, 0);
+        label.setFont("*-*-24");
+        label.setLocation(getWidth()/2-label.getWidth()/2,  getHeight()/2-label.getHeight()/2);
+        label.setColor(Color.green);
+        add(label);
+    }
+
+    private void initializeTiles()
+    {
+        for (int r=0; r<tiles.length; r++)
+        {
+            for (int c=0; c<tiles[0].length; c++)
+            {
+                tiles[r][c] = new GRect(c*TILE_SIZE, r*TILE_SIZE, TILE_SIZE, TILE_SIZE);
+                tiles[r][c].setFilled(true);
+                if (Math.random()<0.5)
+                    tiles[r][c].setColor(firstColor);
+                else
+                    tiles[r][c].setColor(secondColor);
+                add(tiles[r][c]);
+            }
+        }
+    }
+
+    private int playGame()
+    {
+        while (true)
+        {
+            pause(25); 
+            if (allSameColor())  return 0;
+            if (checkerBoard())  return 1;
+            //if (ring())          return 2;
+            //if (stripes())       return 3;
+        }
+    }
+
+    private boolean allSameColor() 
+    {
+        GRect startTile = tiles[0][0];
+        for (int r=0; r<tiles.length; r++)
+            for (int c=0; c<tiles[0].length; c++)
+                if (!tiles[r][c].equals(startTile))
+                    return false;
+        return true;
+    }
+
+    private boolean checkerBoard() 
+    {
+        return false; // we'll do this
+    }
+
+    private boolean ring() 
+    {
+       return false;  // we'll do this
+    }
+
+    private boolean stripes()
+    {
+        return false; // we'll do this
+    }
+
+    private void toggleTileColor(GRect tile)
+    {
+        if (tile.getColor().equals(firstColor))
+            tile.setColor(secondColor);
+        else
+            tile.setColor(firstColor);   
+    }
+
+    public void mouseClicked(MouseEvent event)
+    {
+        if (status != -1)
+            return;
+        int row = event.getY()/TILE_SIZE;
+        int col = event.getX()/TILE_SIZE;
+        toggleTileColor(tiles[row][col]);
+        if (!isShiftKeyDown(event))
+        {
+            if (row!=0)
+                toggleTileColor(tiles[row-1][col]);
+            if (row!=tiles.length)
+                toggleTileColor(tiles[row+1][col]);
+            if (col!=0)
+                toggleTileColor(tiles[row][col-1]);
+            if (col!=tiles.length-1)
+                toggleTileColor(tiles[row][col+1]);
+        }
+
+    }
+
+    private boolean isShiftKeyDown(MouseEvent event)
+    {
+        return (event.getModifiers() & MouseEvent.SHIFT_MASK) != 0;
+    }
+}
