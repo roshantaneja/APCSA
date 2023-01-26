75ad3bfa3bc0c70e4b1dcf8948af36f64076de8a
Daroshi11260 <rytaneja@gmail.com>
2023-01-26 11:14:24 -0800

    done bogglish

--- Unit4_Multiplechoice_2Darrays/Bogglish2023/Bogglish.java
+++ Unit4_Multiplechoice_2Darrays/Bogglish2023/Bogglish.java
@@ -10,6 +10,39 @@ public class Bogglish extends ConsoleProgram
         setFont("*-*-24");
         String[][] board = new String[5][5];
         // version 0.1, 0.2, and 0.5 here
+        String letters = ALPHABET;
+        for (int r = 0; r < board.length; r ++){
+            for (int c = 0; c < board[0].length; c++){
+                int index = (int)(Math.random() * letters.length());
+                board[r][c]= letters.substring(index, index+1);
+                letters = letters.substring(0,index) + letters.substring(index+1);
+            }
+        }
+        printBoard(board);
+        String word = readLine("enter a word: ");
+        while (word.length() > 0){
+            if(checkWord(word, board)){
+                println("Yes, '" + word + "' is a word on the board");
+            } else {
+                println("Ur Bad");
+            }
+            word = readLine("enter a word: ");
+        }
+        ArrayList<String> words = Dictionary.getAllWords();
+        for (String w: words){
+            if (checkWord(w, board)){
+                println(w);
+            }
+        }
+    }
+    
+    public void printBoard(String[][] board){
+        for (int r = 0; r < board.length; r ++){
+            for (int c = 0; c < board[0].length; c++){
+                print(board[r][c] + " ");
+            }
+            println();
+        }
     }
 
     /** Returns true if the word is "contained" somewhere in the board,
@@ -20,8 +53,19 @@ public class Bogglish extends ConsoleProgram
      */
     private boolean checkWord(String word, String[][] board)
     {
-        // version 0.4 here
-        return false;
+        word = word.toUpperCase();
+        if (word.length() < 3){ return false; }
+        for (int i = 0; i < word.length()-1; i ++){
+            String first = word.substring(i, i+1);
+            String second = word.substring(i+1, i+2);
+            Position firstPosition = getLetterPosition(first, board);
+            Position secondPosition = getLetterPosition(second, board);
+            if (Math.abs(firstPosition.getRow() - secondPosition.getRow()) > 1 || 
+                Math.abs(firstPosition.getCol() - secondPosition.getCol()) > 1){
+                return false;
+            }
+        }
+        return true;
     }
 
     /** Returns the Position of where the letter is in the board
@@ -32,8 +76,14 @@ public class Bogglish extends ConsoleProgram
      */
     private Position getLetterPosition(String letter, String[][] board)
     {
-        // version 0.3 here
-        return null;
+        for (int r = 0; r < board.length; r ++){
+            for (int c = 0; c < board[0].length; c++){
+                if (board[r][c].equals(letter)){
+                    return new Position(r, c);
+                }
+            }
+        }
+        return new Position(-100, -100);
     }
 
 }
---
+++ Unit4_Multiplechoice_2Darrays/Bogglish2023/Position.java
@@ -0,0 +1,21 @@
+//Roshan Taneja
+
+public class Position
+{
+    private int row;
+    private int col;
+    
+    public Position(int r, int c)
+    {
+        row = r;
+        col = c;
+    }
+    
+    public int getRow(){
+        return col;
+    }
+    
+    public int getCol(){
+        return row;
+    }
+}
