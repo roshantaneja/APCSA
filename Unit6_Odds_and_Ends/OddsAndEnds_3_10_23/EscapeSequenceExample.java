7da1712ec79bd4dc37678b7063284a8d24fa2c7d
Daroshi11260 <rytaneja@gmail.com>
2023-03-10 10:34:02 -0800

    good for marty

---
+++ Unit6_Odds_and_Ends/OddsAndEnds_3_10_23/EscapeSequenceExample.java
@@ -0,0 +1,6 @@
+public class EscapeSequenceExample{
+
+    public static void main(String[] args){
+        System.out.println("if you want to print a \", you must use \\\".");
+    }
+}
---
+++ Unit6_Odds_and_Ends/OddsAndEnds_3_10_23/FinalExample.java
@@ -0,0 +1,6 @@
+public class FinalExample {
+    public static void main(String[] args){
+        final int num = 0;
+        System.out.println(num);
+    }
+}
