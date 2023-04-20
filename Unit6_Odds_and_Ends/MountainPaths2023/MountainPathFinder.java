1889509dfe31805135a4b5a9557a61147f6e1406
Daroshi11260 <rytaneja@gmail.com>
2023-04-20 14:05:07 -0700

    mountain stuffs

--- Unit6_Odds_and_Ends/MountainPaths2023/MountainPathFinder.java
+++ Unit6_Odds_and_Ends/MountainPaths2023/MountainPathFinder.java
@@ -30,6 +30,32 @@ public class MountainPathFinder extends GraphicsProgram
     private void initGUI()
     {
         // you'll do this in task #0
+        JLabel fileNameLabel = new JLabel("File Name:");
+        add(fileNameLabel, NORTH);
+        fileNameField = new JTextField("Colorado_480x480.txt", 18);
+        add(fileNameField, NORTH);
+        JLabel sizeLabel = new JLabel("Map Size:");
+        add(sizeLabel, NORTH);
+        
+        rowsField = new JTextField("480", 3);
+        add(rowsField, NORTH);
+        
+        JLabel xLabel = new JLabel("x");
+        add(xLabel, NORTH);
+        
+        colsField = new JTextField("480", 3);
+        add(colsField, NORTH);
+        
+        loadButton = new JButton("Load File");
+        add(loadButton, NORTH);
+        
+        drawButton = new JButton("Draw Map");
+        drawButton.setEnabled(false);
+        add(drawButton, NORTH);
+        
+        findPathButton = new JButton("Find Path");
+        findPathButton.setEnabled(false);
+        add(findPathButton, NORTH);
     }
 
     public void actionPerformed(ActionEvent event)
@@ -42,6 +68,18 @@ public class MountainPathFinder extends GraphicsProgram
     private void handleLoadButton()
     {
         // you'll do this in task #2
+        removeAll();
+        String fileName = fileNameField.getText();
+        int rows = Integer.parseInt(rowsField.getText());
+        int cols = Integer.parseInt(colsField.getText());
+        mountainMap = new TopographicMap(fileName, rows, cols);
+        
+        if (mountainMap.isValid()){
+            JOptionPane.showMessageDialog(this, "Map Loaded Successfully!");
+            drawButton.setEnabled(true);
+        } else {
+            JOptionPane.showMessageDialog(this, "No bueno");
+        }
     }
 
 
--- Unit6_Odds_and_Ends/MountainPaths2023/TopographicMap.java
+++ Unit6_Odds_and_Ends/MountainPaths2023/TopographicMap.java
@@ -9,30 +9,70 @@ public class TopographicMap
     private double[][] mapData;
     private boolean valid;
 
-    public TopographicMap(String filename, int rows, int cols)
+    public TopographicMap(String fileName, int rows, int cols)
     {
         // you'll do this in task #1
+        valid = true;
+        mapData = new double[rows][cols];
+        double[] data = FileHelper.readDataFromFile(fileName, rows * cols);
+        if (data.length == 0){
+            valid = false;
+        } else {
+            //use data to fill mapdata
+            int index = 0;
+            for (int i = 0; i < rows; i++){
+                for (int j = 0; j < cols; j++){
+                    mapData[i][j] = data[index];
+                    index++;
+                }
+            }
+        }
     }
 
     public boolean isValid() {return valid;}
     
     private double findMinimum()
     {
-        // you'll do this in task #3
-        return 0; // just to get it to compile
+        double min = mapData[0][0];
+        for (for int i = 0; i < mapData.length; i++){
+            for (int j = 0; j < mapData[i].length; j++){
+                if (mapData[i][j] < min){
+                    min = mapData[i][j];
+                }
+            }
+        }
+        return min; // just to get it to compile
     }
 
     private double findMaximum()
     {
-        // you'll do this in task #3
-        return 0; // just to get it to compile
+        double max = mapData[0][0];
+        for (int i = 0; i < mapData.length; i++){
+            for (int j = 0; j < mapData[i].length; j++){
+                if (mapData[i][j] > max){
+                    max = mapData[i][j];
+                }
+            }
+        }
+        return max;
     }
 
     public void drawMap(GraphicsProgram graphics)
     {
-
         double min = this.findMinimum();
         double max = this.findMaximum();
+
+
+        for (int i = 0; i < mapData.length; i++){
+            for (int j = 0; j < mapData[i].length; j++){
+                double value = mapData[i][j];
+                double scaledValue = (value - min) / (max - min);
+                GRect rect = new GRect(j, i, 1, 1);
+                rect.setFilled(true);
+                rect.setFillColor(new Color((float)scaledValue, (float)scaledValue, (float)scaledValue));
+                graphics.add(rect);
+            }
+        }
 
         // you'll do this in task #4
     }
